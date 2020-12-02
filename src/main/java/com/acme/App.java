bbaa108ee9896698aadf830977c725d5bc0bfaca
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2020-12-02 15:39:20 -0600

    Reapply newly updated gitignore

---
+++ src/main/java/com/acme/App.java
@@ -0,0 +1,20 @@
+package com.acme;
+
+public class App {
+
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public String getGreeting4() {
+        return "Hello world.";
+    }
+
+    public String getGreeting5() {
+        return "Hello world.";
+    }
+
+    public static void main() {
+        System.out.println(new App().getGreeting());
+    }
+}
---
+++ src/main/java/com/acme/R_AP_DISB_RQSTImpl.java
@@ -0,0 +1,4 @@
+package com.acme;
+
+public class R_AP_DISB_RQSTImpl {
+}
