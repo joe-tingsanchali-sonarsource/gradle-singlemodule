beda64b567ce3ca0a9362e73697861263bf120bc
Administrator <joe.tingsanchali@sonarsource.com>
2020-12-03 16:53:30 -0600

    whatever it was me

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -9,6 +9,10 @@ public class App {
         return "whatever";
     }
 
+    public String getGreeting3() {
+        return "whateve2r";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
