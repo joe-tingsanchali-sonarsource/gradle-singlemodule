063c67522107f335fad217fd9fc16354bac0957c
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2020-12-03 16:47:41 -0600

    whatever it was me

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -5,6 +5,10 @@ public class App {
         return "Hello world.";
     }
 
+    public String getGreeting2() {
+        return "whatever";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
