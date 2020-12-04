221f3d2fecd41f179cb39f1d6ac64915c8d43de2
joe.tingsanchali@sonarsource.com <joe.tingsanchali@sonarsource.com>
2020-12-04 09:16:02 -0600

    Test

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -5,30 +5,6 @@ public class App {
         return "Hello world.";
     }
 
-    public String getGreeting2() {
-        return "whatever";
-    }
-
-    public String getGreeting3() {
-        return "whateve2r";
-    }
-
-    public String getGreeting4() {
-        return "whateve2r";
-    }
-
-    public String getGreeting5() {
-        return "whateve2r";
-    }
-
-    public String getGreeting6() {
-        return "whateve2r";
-    }
-
-    public String getGreeting7() {
-        return "whateve2r";
-    }
-
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
