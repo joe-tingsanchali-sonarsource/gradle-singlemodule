f7b73096eb67e28f5852ee9598ddc65d4bf6e0ae
joe.tingsanchali@sonarsource.com <joe.tingsanchali@sonarsource.com>
2020-12-04 09:11:40 -0600

    Test test admin

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -13,6 +13,22 @@ public class App {
         return "whateve2r";
     }
 
+    public String getGreeting4() {
+        return "whateve2r";
+    }
+
+    public String getGreeting5() {
+        return "whateve2r";
+    }
+
+    public String getGreeting6() {
+        return "whateve2r";
+    }
+
+    public String getGreeting7() {
+        return "whateve2r";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
